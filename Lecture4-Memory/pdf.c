81b9a8e3ee6e07e98344fe6265c87ead72aaef7d
raulkrivan <krivanrauladrian@gmail.com>
2024-03-22 12:05:50 +0200

    studies 22/03/2024

---
+++ Lecture4-Memory/pdf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[])
+{
+    char *filename = argv[1];
+
+    FILE *f = fopen(filename, "r");
+    if (f == NULL)
+    {
+        return 1;
+    }
+
+    uint8_t buffer[4];
+    fread(buffer, 1, 4, f);
+
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%u\n", buffer[i]);
+    }
+    fclose(f);
+}
