6c3b4a50f3002c99418060e4fd4d1b3217b47961
raulkrivan <krivanrauladrian@gmail.com>
2024-03-22 10:03:40 +0200

    playing around with fizz_buzz.c

---
+++ fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void fizz_buzz();
+
+int main(void)
+{
+    fizz_buzz();
+}
+
+void fizz_buzz()
+{
+    int n = 100;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("%d FizzBuzz\n", i);
+        }
+        else if (i % 3 == 0)
+        {
+            printf("%d Fizz\n", i);
+        }
+        else if (i % 5 == 0)
+        {
+            printf("%d Buzz\n", i);
+        }
+        else
+        {
+            printf("%d:\n", i);
+        }
+    }
+}
